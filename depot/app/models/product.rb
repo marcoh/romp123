9ad605f48558d3faf0667c3212b0c44b9361d388
Marco Heerebout <marco.heerebout@gmail.com>
2009-05-07 10:26:17 +0000

    Klaar met Task A

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
 
   validates_presence_of :title, :description, :image_url
+  validates_length_of :title, :minimum => 10
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :message => "already exists, must be unique."
   validates_format_of :image_url,
                       :with => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for GIF, JPG or PNG image.'
