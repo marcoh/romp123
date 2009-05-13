374d39e9d4104fdaff1c31de7334f3dcf897550e
Marco Heerebout <marco.heerebout@gmail.com>
2009-05-13 14:14:39 +0000

    Klaar met Task G

---
+++ depot/app/controllers/info_controller.rb
@@ -0,0 +1,19 @@
+class InfoController < ApplicationController
+  def who_bought
+    @product = Product.find(params[:id])
+    @orders = @product.orders
+
+    respond_to do |format|
+      format.html
+      format.atom { render :layout => false }
+      format.xml { render :layout => false }
+      format.json {render :layout => false, :json => @product.to_json(:include => :orders)}
+    end
+  end
+
+  #PROTECTED
+  def authorize
+    
+  end
+
+end
